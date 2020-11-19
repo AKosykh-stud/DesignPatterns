532fa88f5c410cfd615a1aa4bfc0dbce7cb65c8a
Andrew <Apased@ukr.net>
2020-11-20 00:04:03 +0200

    Added some comments to code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var service = new MessageService();
-        service.AddDateToMessage("Message from original service");
+        var service = new MessageService(); // Creates service instance
+        service.AddDateToMessage("Message from original service"); // Adds date to our message
 
-        var adapter = new MessageServiceAdapter(service);
-        var resultOfAdapting = adapter.AddAuthorToMessage("Message from adapted service", "Author_1");
+        var adapter = new MessageServiceAdapter(service); // Creates adapter instance
+        var resultOfAdapting = adapter.AddAuthorToMessage("Message from adapted service", "Author_1"); // Adapts message by adding author name
 
         System.out.println(resultOfAdapting);
     }
